fe907e087df34cff77c5fe926a8c139e85e684fb
mukang <mukang.max@foxmail.com>
2018-07-25 17:46:18 +0800

    new

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by 穆康 on 2018/7/25.
+//  Copyright © 2018年 穆康. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, world", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
