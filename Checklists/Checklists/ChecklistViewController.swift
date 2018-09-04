ae725f780635d0d49718a9d94eb246433f70acd8
mukang <mukang.max@foxmail.com>
2018-09-04 17:37:54 +0800

    Chapter 9: Table Views

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by 穆康 on 2018/9/4.
+//  Copyright © 2018年 穆康. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+}
+
